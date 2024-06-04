2af71d61d10ebdcb7bcc8c4b5f51138c56cd5689
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2024-06-04 23:52:32 +0100

    docs: Setup the project's folder/file boilerplate structure

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+gsap.registerPlugin(CustomEase, Flip);
+CustomEase.create("ease-out-cubic", "0.215,0.61,0.355,1");
+
+function initializeAnimation() {}
+
+window.addEventListener("DOMContentLoaded", initializeAnimation);
+
+// Uncomment to be able to toggle the grid visualizer
+document.addEventListener("keydown", (event) => {
+  if (event.shiftKey && event.key.toLowerCase() === "g") {
+    document.querySelector(".overlay").classList.toggle("active");
+  }
+});
