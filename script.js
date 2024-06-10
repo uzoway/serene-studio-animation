5c18092f5957374b7cd85a5436653d7e35d8dc8f
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2024-06-10 13:03:06 +0100

    feat: Install and initialize lenis smooth.

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 "use strict";
 
+// Register GSAP customeease and flip plugin
 gsap.registerPlugin(CustomEase, Flip);
 CustomEase.create("ease-out-cubic", "0.215,0.61,0.355,1");
 
-function initializeAnimation() {}
+// Animation for the works page
+// function initializeAnimation() {}
 
-window.addEventListener("DOMContentLoaded", initializeAnimation);
+// Run animation when dom is ready
+// window.addEventListener("DOMContentLoaded", initializeAnimation);
 
-// Uncomment to be able to toggle the grid visualizer
+// Toggle the grid visualizer
 document.addEventListener("keydown", (event) => {
   if (event.shiftKey && event.key.toLowerCase() === "g") {
     document.querySelector(".overlay").classList.toggle("active");
   }
 });
+
+// Lenis smooth scroll
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
